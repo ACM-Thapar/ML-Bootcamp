310c140ba8e6394de0b8002b7f869a86418b562c
Abhinav Garg <91721449+theabhiinavgarg@users.noreply.github.com>
2022-10-05 21:45:16 +0530

    added functionality for checkboxes

--- script.js
+++ script.js
@@ -31,3 +31,19 @@ $(document).mouseup(function (e) {
       tgt.removeClass("active");
 });
 
+var checkboxValues = JSON.parse(localStorage.getItem("checkboxValues")) || {},
+  $checkboxes = $("#checkbox-container :checkbox");
+
+$checkboxes.on("change", function () {
+  $checkboxes.each(function () {
+    checkboxValues[this.id] = this.checked;
+  });
+
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+// On page load
+$.each(checkboxValues, function (key, value) {
+  $("#" + key).prop("checked", value);
+});
+
